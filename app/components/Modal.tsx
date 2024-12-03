e5bf508fb3db69f3f7a6481e03120be03dff524a
jsimpson15011 <joseph.franklin.simpson@gmail.com>
2024-12-02 22:22:11 -0700

    Add modal close to mobile

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,9 +1,11 @@
 "use client"
 import {Media, Modal, Page} from "@/app/types/payloadTypes";
-import {useCallback, useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import Image from "next/image";
 import renderText from "@/app/components/BlockRenderer/utils/renderText";
 import Link from "next/link";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faXmark} from "@awesome.me/kit-2a2dc088e2/icons/classic/regular";
 
 const ModalComponent = ({modal}: { modal: Modal }) => {
     const [isOpen, setIsOpen] = useState(true);
@@ -49,16 +51,24 @@ const ModalComponent = ({modal}: { modal: Modal }) => {
             <div className="flex flex-wrap gap-4 justify-center w-full p-2">
                 {
                     typeof secondaryAction === "string" && secondaryAction ?
-                        <a className="px-16 py-3 text-xl hover:bg-gray-50" href={secondaryAction as string}>{modal.secondaryAction?.title}</a> :
-                        <Link className="px-16 py-3 text-xl hover:bg-gray-50" href={`/${(secondaryAction as Page).full_path}`}>{modal.secondaryAction?.title}</Link>
+                        <a className="px-16 py-3 text-xl hover:bg-gray-50"
+                           href={secondaryAction as string}>{modal.secondaryAction?.title}</a> :
+                        <Link className="px-16 py-3 text-xl hover:bg-gray-50"
+                              href={`/${(secondaryAction as Page).full_path}`}>{modal.secondaryAction?.title}</Link>
                 }
                 {
                     typeof primaryAction === "string" && primaryAction ?
-                        <a className="px-16 py-3 bg-brand-yellow hover:bg-brand-yellow/80 font-black uppercase text-xl" href={primaryAction as string}>{modal.primaryAction?.title}</a> :
-                        <Link className="px-16 py-3 bg-brand-yellow hover:bg-brand-yellow/80 font-black uppercase text-xl" href={`/${(primaryAction as Page).full_path}`}>{modal.primaryAction?.title}</Link>
+                        <a className="px-16 py-3 bg-brand-yellow hover:bg-brand-yellow/80 font-black uppercase text-xl"
+                           href={primaryAction as string}>{modal.primaryAction?.title}</a> :
+                        <Link
+                            className="px-16 py-3 bg-brand-yellow hover:bg-brand-yellow/80 font-black uppercase text-xl"
+                            href={`/${(primaryAction as Page).full_path}`}>{modal.primaryAction?.title}</Link>
                 }
             </div>
         </div>
+        <button className="absolute right-4 top-4 z-50 bg-white bg-opacity-50 flex items-center justify-center p-4" onClick={() => setIsOpen(false)}>
+            <FontAwesomeIcon className="size-5" icon={faXmark}/>
+        </button>
         <div onClick={() => setIsOpen(false)} className="inset-0 bg-black bg-opacity-50 z-40 fixed"/>
     </div>
 }
