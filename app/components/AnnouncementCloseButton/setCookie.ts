b02b38892b1b6e4c15d7630a83ed103665efc013
Joe simpson <joe@egmrc.com>
2025-08-13 08:59:06 -0600

    Redirect after setting `hide-banner` cookie to resolve `Response.clone` issue

--- app/components/AnnouncementCloseButton/setCookie.ts
+++ app/components/AnnouncementCloseButton/setCookie.ts
@@ -1,7 +1,11 @@
 "use server";
 import { cookies } from "next/headers";
+import {redirect} from "next/navigation";
 
 export async function setCookie(updatedAt: string) {
     const monthInSeconds = 2678400;
     cookies().set("hide-banner", updatedAt, {maxAge: monthInSeconds});
+
+    // Redirect to the same page to avoid the Response.clone issue
+    redirect('');  // Empty string redirects to current URL
 }
